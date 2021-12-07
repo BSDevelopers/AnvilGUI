5662a1d8257786d8cba4f51106fd73c2748f5f5b
brainsynder <briansnyder498@gmail.com>
2021-12-07 00:39:33 -0500

    Implemented some of the old code to keep them shaded in when compiling with simplepets

--- api/src/main/java/net/wesjd/anvilgui/version/VersionMatcher.java
+++ api/src/main/java/net/wesjd/anvilgui/version/VersionMatcher.java
@@ -2,6 +2,9 @@ package net.wesjd.anvilgui.version;
 
 import org.bukkit.Bukkit;
 
+import java.util.Arrays;
+import java.util.List;
+
 /**
  * Matches the server's NMS version to its {@link VersionWrapper}
  *
@@ -9,6 +12,10 @@ import org.bukkit.Bukkit;
  * @since 1.2.1
  */
 public class VersionMatcher {
+    private final List<Class<? extends VersionWrapper>> versions = Arrays.asList(
+            Wrapper1_17_R1.class,
+            Wrapper1_18_R1.class
+    );
 
     /**
      * Matches the server version to it's {@link VersionWrapper}
@@ -19,11 +26,12 @@ public class VersionMatcher {
     public VersionWrapper match() {
         final String serverVersion = Bukkit.getServer().getClass().getPackage().getName().split("\\.")[3].substring(1);
         try {
-            return (VersionWrapper) Class.forName(getClass().getPackage().getName() + ".Wrapper" + serverVersion).newInstance();
+            return versions.stream()
+                    .filter(version -> version.getSimpleName().substring(7).equals(serverVersion))
+                    .findFirst().orElseThrow(() -> new RuntimeException("Your server version isn't supported in AnvilGUI!"))
+                    .newInstance();
         } catch (IllegalAccessException | InstantiationException exception) {
             throw new IllegalStateException("Failed to instantiate version wrapper for version " + serverVersion, exception);
-        } catch (ClassNotFoundException exception) {
-            throw new IllegalStateException("AnvilGUI does not support server version \"" + serverVersion + "\"", exception);
         }
     }
 
